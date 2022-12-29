3fc95805229e10790b5d174f73cb7d2cf54639e9
Felipe <antoniofelipesilva@live.com>
2022-12-29 19:19:17 -0300

    version.1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { multiplicar, somar, subtrair } from './Matematica';
 import validator from 'validator';
+let email: string = 'felipe@gmail.com';
 
-let n1: number = 25;
-let n2: number = 30;
-
-console.log(validator.isEmail('felipe@gmail.com'))
-
-console.log(`Multiplica: ${multiplicar(n1, n2)}`)
-console.log(`Somar ${somar(n1, n2)}`)
-console.log(`Subtrair: ${subtrair(n1, n2)}`)
+if(validator.isEmail(email)){
+  console.log('Email Validor!')
+}else{
+  console.log('Email invalidor!')
+}
