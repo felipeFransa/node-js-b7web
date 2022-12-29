3b280022576f4a930e8aa1036954048c65ef8795
Felipe <antoniofelipesilva@live.com>
2022-12-29 19:09:47 -0300

    version.1.0.2

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Matematica_1 = require("./Matematica");
+const validator_1 = __importDefault(require("validator"));
 let n1 = 25;
 let n2 = 30;
+console.log(validator_1.default.isEmail('felipe@gmail.com'));
 console.log(`Multiplica: ${(0, Matematica_1.multiplicar)(n1, n2)}`);
 console.log(`Somar ${(0, Matematica_1.somar)(n1, n2)}`);
 console.log(`Subtrair: ${(0, Matematica_1.subtrair)(n1, n2)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { multiplicar, somar, subtrair } from './Matematica'
+import { multiplicar, somar, subtrair } from './Matematica';
+import validator from 'validator';
 
 let n1: number = 25;
 let n2: number = 30;
+
+console.log(validator.isEmail('felipe@gmail.com'))
 
 console.log(`Multiplica: ${multiplicar(n1, n2)}`)
 console.log(`Somar ${somar(n1, n2)}`)
